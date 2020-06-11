a15a551720fdba97393e2be236ced2d21cf8c09a
victorernan <victor.ospina.giraldo@gmail.com>
2020-06-11 17:03:52 +0200

    cambios

---
+++ index.php
@@ -0,0 +1,87 @@
+<?php      
+include 'menu.html'; 
+?>
+   
+        <div class="row" id="contenido">
+            <div class="col-md-1">&nbsp;</div>
+            <div class="card">
+                <img src="images/logo2.jpg" class="card-img-top" alt="foto">
+                <div class="card-body">
+                    <h5 class="card-title">Patronaje</h5>
+                    <p class="card-text">Aqui iria una breve explicacion de lo que trata el curso y de los contenidos y
+                        aprendizajes que se llevan acabo en el.</p>
+                    <button class="boton-cards">Ir al curso</button>
+                </div>
+            </div>
+            <div class="col-md-1">&nbsp;</div>
+            <div class="card">
+                <img src="images/logo2.jpg" class="card-img-top" alt="foto">
+                <div class="card-body">
+                    <h5 class="card-title">Diseño de moda</h5>
+                    <p class="card-text">Aqui iria una breve explicacion de lo que trata el curso y de los contenidos y
+                        aprendizajes que se llevan acabo en el.</p>
+                    <button class="boton-cards">Ir al curso</button>
+                </div>
+            </div>
+            <div class="col-md-1">&nbsp;</div>
+            <div class="card">
+                <img src="images/logo2.jpg" class="card-img-top" alt="foto">
+                <div class="card-body">
+                    <h5 class="card-title">Corte y Confeccion</h5>
+                    <p class="card-text">Aqui iria una breve explicacion de lo que trata el curso y de los contenidos y
+                        aprendizajes que se llevan acabo en el.</p>
+                    <button class="boton-cards">Ir al curso</button>
+                </div>
+            </div>
+            <div class="col-md-1">&nbsp;</div>
+            <div class="card">
+                <img src="images/logo2.jpg" class="card-img-top" alt="foto">
+                <div class="card-body">
+                    <h5 class="card-title">Modelaje</h5>
+                    <p class="card-text">Aqui iria una breve explicacion de lo que trata el curso y de los contenidos y
+                        aprendizajes que se llevan acabo en el.</p>
+                    <button class="boton-cards">Ir al curso</button>
+                </div>
+            </div>
+        </div>
+
+        <div class="fixed-bottom" style="background-color: black; color:rgb(97, 96, 96); text-align: center;">
+            &copy;2020-2021 TFG DESARROLLO DE
+            APLICACIONES
+            WEB todos los derechos reservados
+            <div class="rrss">
+                <a class="btn btn-block btn-social btn-twitter">
+                    <span class="fa fa-twitter"></span> Siguenos en Twitter
+                </a>
+                <a class="btn btn-block btn-social btn-twitter">
+                    <span class="fa fa-facebook"></span> Siguenos en Facebook
+                </a>
+                <a class="btn btn-block btn-social btn-twitter">
+                    <span class="fa fa-instagram"></span> Siguenos en instagram
+                </a>
+            </div>
+
+        </div>
+
+    </div>
+
+
+    <!-- Optional JavaScript -->
+
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"
+        integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n"
+        crossorigin="anonymous"></script>
+
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
+        integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo"
+        crossorigin="anonymous"></script>
+
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"
+        integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6"
+        crossorigin="anonymous"></script>
+
+</body>
+
+</html>
