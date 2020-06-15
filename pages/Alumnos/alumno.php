e458827a173acd4c48af899a18510e4ee4b7d22f
DavidRojas94 <davidrojascaballero94@gmail.com>
2020-06-15 21:08:37 +0200

    archivo alumno

--- pages/Alumnos/alumno.php
+++ pages/Alumnos/alumno.php
@@ -14,39 +14,42 @@
 
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
         integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-    <link rel="stylesheet" href="../css/font-awesome.css">
-    <link rel="stylesheet" href="../css/master.css">
-    <title>TFG</title>
-    <style>
-        .jumbotron-fluid {
-            background-color: rgb(199, 199, 88);
-        }
-        #barra {
-          margin-bottom: 0%;
-        }
+    <link rel="shortcut icon" href="ico/favicon.ico">
+    <link rel="stylesheet" href="css/font-awesome.css">
+    <link rel="stylesheet" type="text-css" href="css/bootstrap-social.scss">
+    <link rel="stylesheet" type="text/css" href="fonts/Leixo.ttf">
 
-    </style>
+    <title>TFG</title>
+
 </head>
 
 <body>
     <div class="container-fliud">
         <div class="row" id="barra">
             <div class="col-md-1">&nbsp</div>
-            <div class="col-md-2"><img src="../images/Logo.png" id="icono" /></div>
-            <div class="col-md-2" style="margin-top: 2%;"><span id="titulo">Lerning Sewing Desing</span></div>
+            <div class="col-md-2"><img src="images/Logo.png" id="icono" /></div>
+            <div class="col-md-2" style="margin-top: 2%;"><span id="titulo">Learning Sewing Desing</span></div>
             <div class="col-lg-1 col-md-1 col-sm-1">
             </div>
             <div class="col-lg-1 col-md-1 col-sm-1">
             </div>
             <div class="col-md-2">&nbsp</div>
         </div>
-        <div class="jumbotron jumbotron-fluid">
-            <div class="container">
-                <h1 class="display-4">Mis cursos</h1>
+        <div class="container-fluid">
+            <h1 class="display-4">Mis cursos</h1>
+            <div class="row">
+                <div class="col-sm-1">&nbsp;</div>
+        <?php
+            $conexion = mysqli_connect('localhost', 'root', '', 'academiatfg');
+            mysqli_set_charset($conexion, 'utf-8');
+            $consulta="select nombre from curso c, cursos_adquiridos cu where c.idcurso = cu.curso_idcurso;";
+            $resultado=mysqli_query($conexion,$consulta);
+            while($fila=mysqli_fetch_row($resultado)){?>
+                <div class="col-sm-3">
                 <div class="card" style="width: 18rem;">
-                    <img src="../images/logo2.jpg" class="card-img-top" alt="...">
+                    <img src="images/logo2.jpg" class="card-img-top" alt="...">
                     <div class="card-body">
-                        <h1>Patronaje</h1>
+                        <h2><?php echo $fila[0]?></h2>
                         <h6>Tu progreso</h6>
                         <div class="progress">
                             <div class="progress-bar" role="progressbar"
@@ -55,35 +58,20 @@
                                 25%
                             </div>
                         </div>
-                        <a href="#" class="card-link">Seguir viendo</a>
-                        <a href="#" class="card-link">Inicio</a>
+                        <a href='paginacurso.html<?php echo $fila[0] ?>'>Seguir viendo</a>
+                        <a href="" class="card-link">Inicio</a>
                     </div>
                 </div>
-                </p>
             </div>
+           <?php }  
+            mysqli_close($conexion);
+          ?>
         </div>
-
-        <div class="fixed-bottom botomito" style="background-color: black; color:rgb(97, 96, 96); text-align: center;">
-            &copy;2020-2021 TFG DESARROLLO DE
-            APLICACIONES
-            WEB todos los derechos reservados
-            <div class="rrss">
-                <a class="btn btn-block btn-social btn-twitter">
-                    <span class="fa fa-twitter"></span> Siguenos en Twitter
-                </a>
-                <a class="btn btn-block btn-social btn-twitter">
-                    <span class="fa fa-facebook"></span> Siguenos en Facebook
-                </a>
-                <a class="btn btn-block btn-social btn-twitter">
-                    <span class="fa fa-instagram"></span> Siguenos en instagram
-                </a>
-            </div>
-
-        </div>
+        
 
     </div>
-
-
+    </div>
+    
     <!-- Optional JavaScript -->
 
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
